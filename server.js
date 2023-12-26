b6ceb648befbb63ce0a4b04a61b26db081ae293f
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2023-12-26 02:31:36 +0100

    error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.use(express.json());
 
 
 app.get("/", async (req, res) => {
-  res.send("hello from server").json({message:""})
+  res.send("hello from server")
 });
 
 app.listen( 3000, () => console.log("Server is listening on port 3000..."));
