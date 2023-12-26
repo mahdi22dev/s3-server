9a3fe90e8fee459a721d5df55cac297ac68b51e6
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2023-12-26 02:21:44 +0100

    first commitss

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+
+app.get("/", async (req, res) => {
+  res.send("hello from server").json({message:""})
+});
+
+app.listen( 3000, () => console.log("Server is listening on port 3000..."));
