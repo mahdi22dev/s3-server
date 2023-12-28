cc932643babfd9d3a47d658e756dd20a18a6358e
mahdi22dev <154707248+somerandomdev21@users.noreply.github.com>
2023-12-28 01:09:47 +0100

    chore; server instance modification

--- s3server/s3server.js
+++ s3server/s3server.js
@@ -18,8 +18,8 @@ const s3Server = new s3rver({
   ],
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
 s3Server.run();
 console.log("s3 mock server start");
